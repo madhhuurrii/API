72177f2fa90dbdf24d889b261cab88cc9c12b5a7
madhhuurrii <madhuriramakrishnan061@gmail.com>
2021-12-12 00:48:35 +0530

    react assignment done

--- api/src/App.js
+++ api/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
-
+import fetch from "node-fetch"
+const api = "http://localhost:3000/data"
 function App() {
+  const [data, setData] = useState([])
+  const getData =()=>{
+    fetch(api,{
+      method : "GET",
+      headers :{
+        "Content-Type": "application/json"
+      }
+    }).then((res)=>{
+      return res.json()
+    }).then((json)=>{
+      json.sort((a,b)=>{
+          if(parseInt(a.id)>parseInt(b.id)){
+            return 1
+          }
+          else if(parseInt(a.id)<parseInt(b.id)){
+            return -1
+          }
+          else{
+            return 0
+          }
+      })
+      setData(json)
+    })
+  }
+  useEffect(() => {
+    getData()},[])
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {
+         data && data.length>0 && data.map((item)=><p>{item.id} &nbsp;{item.name}</p>)
+       }
     </div>
   );
 }
