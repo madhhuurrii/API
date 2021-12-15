9841b9983d96edcafcadd1766aa5d44e9d3ec753
madhhuurrii <madhuriramakrishnan061@gmail.com>
2021-12-15 09:05:48 +0530

    format code

--- api.mjs
+++ api.mjs
@@ -1,29 +1,26 @@
-var api = "http://localhost:3000/data"
-// var api
-// api.addEventListener('submit',myfunc)
+var api = 'http://localhost:3000/data'
+
 // json-server --watch db.json
-// function myfunc(event){
-// const apis = document.getElementById("api").value;
-  
-import fetch from "node-fetch"
+
+import fetch from 'node-fetch'
 
 fetch(api)
-.then((res)=>{
-    return res.json();
-})
-.then((json)=>{
-   json.sort((a,b)=>{
-        if(parseInt(a.id)>parseInt(b.id)){
-            return 1;
-        }
-        else if(parseInt(a.id)<parseInt(b.id)){
-            return -1;
-        }
-        else{
-            return 0;
-        }
+  .then((res) => {
+    return res.json()
+  })
+  .then((json) => {
+    json.sort((a, b) => {
+      if (parseInt(a.id) > parseInt(b.id)) {
+        return 1
+      } else if (parseInt(a.id) < parseInt(b.id)) {
+        return -1
+      } else {
+        return 0
+      }
     })
     console.log(json)
-    
-})
+  })
+  .catch((err) => {
+    console.log('Error!')
+  })
 // }
--- api/src/App.js
+++ api/src/App.js
@@ -1,60 +1,70 @@
-import React, {useState, useEffect} from 'react'
-import './App.css';
-import fetch from "node-fetch"
-const api = "http://localhost:3000/data"
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import fetch from 'node-fetch'
+const api = 'http://localhost:3000/data'
 function App() {
   const [data, setData] = useState([])
-  const getData =()=>{
-    fetch(api,{
-      method : "GET",
-      headers :{
-        "Content-Type": "application/json"
-      }
-    }).then((res)=>{
-      return res.json()
-    }).then((json)=>{
-      json.sort((a,b)=>{
-          if(parseInt(a.id)>parseInt(b.id)){
+  const getData = () => {
+    fetch(api, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        return res.json()
+      })
+      .then((json) => {
+        json.sort((a, b) => {
+          if (parseInt(a.id) > parseInt(b.id)) {
             return 1
-          }
-          else if(parseInt(a.id)<parseInt(b.id)){
+          } else if (parseInt(a.id) < parseInt(b.id)) {
             return -1
-          }
-          else{
+          } else {
             return 0
           }
+        })
+        setData(json)
       })
-      setData(json)
-    })
+      .catch((err) => {
+        console.log('ERROR!')
+      })
   }
   useEffect(() => {
-    getData()},[])
-    
+    getData()
+  }, [])
+
   return (
     <div className="App">
-    <center>
-    <table className="App">
-      <tr>
-        <th><h1>ID &nbsp;</h1></th>
-        <th><h1>Name &nbsp;</h1></th>
-      </tr>
-      <tr>
-        <td>{
-         data && data.length>0 && data.map((item)=><h2>{item.id} &nbsp;</h2>)
-       }</td>
-       <td>
-       {
-         data && data.length>0 && data.map((item)=><h2>{item.name} &nbsp;</h2>)
-       } 
-       </td>
-      </tr>
-    </table>
-    </center>
-     {/* {
+      <center>
+        <table className="App">
+          <tr>
+            <th>
+              <h1>ID &nbsp;</h1>
+            </th>
+            <th>
+              <h1>Name &nbsp;</h1>
+            </th>
+          </tr>
+          <tr>
+            <td>
+              {data &&
+                data.length > 0 &&
+                data.map((item) => <h2>{item.id} &nbsp;</h2>)}
+            </td>
+            <td>
+              {data &&
+                data.length > 0 &&
+                data.map((item) => <h2>{item.name} &nbsp;</h2>)}
+            </td>
+          </tr>
+        </table>
+      </center>
+      {/* {
          data && data.length>0 && data.map((item)=><h2>{item.id} &nbsp; {item.name}</h2>)
        } */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
